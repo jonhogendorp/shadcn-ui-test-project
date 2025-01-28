3cad05aa8c1457e8f644363d9cee4dc610485727
Jon Hogendorp <jonhoog@live.nl>
2025-01-28 16:58:06 +0100

    first commit test shad cn ui playground

---
+++ app/shad-playground/page.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+} from "@/components/ui/dialog";
+
+const shadPlayground = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <Button>Click me </Button>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant='outline'>Edit Profile</Button>
+        </DialogTrigger>
+        <DialogContent className='sm:max-w-[425px]'>
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you are done.
+            </DialogDescription>
+          </DialogHeader>
+          {/* <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div> */}
+          <DialogFooter>
+            <Button type='submit'>Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <Calendar />
+    </div>
+  );
+};
+
+export default shadPlayground;
